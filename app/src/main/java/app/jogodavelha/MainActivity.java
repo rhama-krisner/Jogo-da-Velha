56ca067916e2aa9f24c27400aaa1c5a7464df60b
Rhama Krisner <rhamakrisner@gmail.com>
2023-10-25 21:35:11 -0300

    Criado projeto

---
+++ app/src/main/java/app/jogodavelha/MainActivity.java
@@ -0,0 +1,17 @@
+package app.jogodavelha;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
