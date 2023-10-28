c3f639d555a2bf3557e47e2791f80e4cb014961e
Rhama Krisner <rhamakrisner@gmail.com>
2023-10-27 21:01:45 -0300

    Adicionado release e fonte para o apk

--- app/src/main/java/app/jogodavelha/JogoActivity.java
+++ app/src/main/java/app/jogodavelha/JogoActivity.java
@@ -1,18 +1,12 @@
 package app.jogodavelha;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -96,6 +90,7 @@ public class JogoActivity extends AppCompatActivity {
 
         b.setEnabled(false);
     }
+
 
     public boolean vitoria(int x) {
 
